05016732850d6418ab3409598952a83096455359
ShyamalBand <shyamalbandara1@gmail.com>
2024-03-24 22:36:13 +0530

    Add project files.

---
+++ Hospital2/Doctor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital2
+{
+    internal class Doctor
+    {
+        public string Name {  get; set; }
+        public int Id { get; set; }
+        public string Speciality {  get; set; }
+
+        public Doctor() 
+        {  
+        }
+        public Doctor(string aname,int aid, string aspeciality) 
+        {
+            Name=aname;
+            Id=aid;
+            Speciality=aspeciality;
+        }
+
+        public void greeting(string aname)
+        {
+            Console.WriteLine("Good morning "+aname);
+        }
+
+    }
+}
---
+++ Hospital2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Hospital2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Doctor doctor1 = new Doctor("a",1,"ent");
+         
+
+            Doctor doctor2 = new Doctor("b",2,"ttt");
+            
+
+            Console.WriteLine(doctor2.Name);
+            Console.WriteLine(doctor1.Id);
+            doctor1.greeting(doctor1.Name);
+        }
+    }
+}
